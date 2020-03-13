745db37b6eeb4b2ea425f41a91280357b2ef1f4b
Ayaz Rashidov <ayaz@wizardsdev.com>
2020-03-13 06:12:19 -0700

    Ios

--- app/App.js
+++ app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { StatusBar } from 'react-native';
+import { StatusBar ,SafeAreaView} from 'react-native';
 import SwitchNavigator from './modules/navigation/switchNavigator';
 import { store, persistor } from './modules/redux/store';
 import { colors } from './services/constants';
@@ -8,11 +8,13 @@ import { PersistGate } from 'redux-persist/es/integration/react';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <StatusBar backgroundColor={colors.mainBackground} />
-        <SwitchNavigator />
-      </PersistGate>
-    </Provider>
+    <SafeAreaView style={{color:colors.mainBackground}}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <StatusBar backgroundColor={colors.mainBackground} />
+          <SwitchNavigator />
+        </PersistGate>
+      </Provider>
+    </SafeAreaView>
   );
 };
